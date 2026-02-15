c10422eddb461de038a174abd587db46286a92ed
Banu Paksoy <paksoybanu83@gmail.com>
2026-02-15 11:08:30 +0100

    Simplify share functionality to prompt-only fallback

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -123,7 +123,11 @@ const PostList = ({ posts, onAddPost }) => {
         }));
     };
 
-    const handleShare = async (post) => {
+    const handleShare = (post) => {
+        window.prompt("Share link:", window.location.href);
+    };
+
+    const _old_handleShare = async (post) => {
         console.log("handleShare called for post:", post.id);
         try {
             const username = post.author?.username || "Someone";
@@ -316,7 +320,7 @@ const PostList = ({ posts, onAddPost }) => {
                                     <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
                                     </svg>
-                                    <span>Share</span>
+                                    <span>Share!</span>
                                 </button>
                             </div>
 
