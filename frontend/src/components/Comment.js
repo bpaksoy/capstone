411e4b02be205e8064026d3fa4df8e04ce9e82f4
Banu Paksoy <paksoybanu83@gmail.com>
2026-02-15 11:16:25 +0100

    Change comment like indicator background to primary teal color

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -144,7 +144,7 @@ function Comment({ postId, lastUpdatedComment, onAddPost, user }) {
                                                     {user && <AddReplyModal commentId={comment.id} onAddReply={updateReplies} className="text-xs font-semibold text-gray-500 hover:underline" />}
                                                     {comment.likes_count > 0 && (
                                                         <div className="flex items-center gap-1 text-xs text-gray-500">
-                                                            <div className="p-0.5 bg-black-100 rounded-full">
+                                                            <div className="p-0.5 bg-primary rounded-full">
                                                                 <svg className="w-2 h-2 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1.91l-.01-.01L23 10z" /></svg>
                                                             </div>
                                                             <span>{comment.likes_count}</span>
